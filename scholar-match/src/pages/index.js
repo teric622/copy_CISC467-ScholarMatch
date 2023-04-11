3091199bff1b55a5f3ff934d005f86f6dd61ceb7
awrose <awrose@udel.edu>
2023-04-11 12:12:46 -0400

    this is the layout for the cards on the home screen

--- scholar-match/src/pages/index.js
+++ scholar-match/src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import db from '../firebase.config';
-import Searchbar from "../components/searchBar";
+//import Searchbar from "../components/searchBar";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Button from 'react-bootstrap/Button'
+
   
 const Home = () => {
   const [scholarships, setScholsarships] = useState([]);
@@ -20,12 +25,32 @@ const Home = () => {
   }
   return (
     <div>
-
-      {
-        scholarships.map((scholarship) => (
-          <h1>Test</h1>
-        ))
-      }
+      <div class="container text-center">
+      <div class="row mb-2">
+        {
+          scholarships.map((scholarship) => (
+            <div class="col">
+              <div>
+              <div class = "card text-center">
+                <div class="card-block">
+                  <h4 class = "card-title">Scholarship Name</h4>
+                  <h6>Amount</h6>
+                  <h6><b>Deadline:</b></h6>
+                  <p class="card-text">
+                    Short Description
+                  </p>
+                  </div>
+                  <div class="card-footer">
+                    <Button variant="primary">Apply</Button>
+                    <Button variant="primary">Save</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+        </div>
+      </div>
     </div>
   );
 };
