c40bf015b386045c072ded7bd4d8c8b2e83f8f99
teric622 <tlasecaeric@gmail.com>
2023-04-18 16:06:09 -0400

    front end of login merged with team members work

--- scholar-match/src/pages/login.js
+++ scholar-match/src/pages/login.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Button } from "react-bootstrap";
+import student from "../images/loginStudent.jpg";
+import googleSignIn from "../images/google.png";
+import logo from "../images/logo.png"
+import "../styling_sheets/loginPage.css"
+
   
 const Login = () => {
   return (
-    <div>
-      <h1>Welcome to ScholarMatch</h1>
+    <div className='x'>
+      <div className='area'>
+      <img src={student} className="image1" />
+
+      <div className='signinarea'>
+            <div className='usersigninformarea'>
+            <img src={logo} className="logoimg" />
+            <h2 class='tellSignIn'>Sign in:</h2>
+            <div class="form-group">
+                    <h3 for="exampleInputEmail1">Email address</h3>
+                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            </div>
+            
+            <div class="form-group">
+              <h3 for="exampleInputPassword1">Password</h3>
+              <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+
+        <div className='signInAuth'>
+        <button type="submit" class="loginclick">Submit</button>
+        </div>
+
+        <div className='forgotPassword'>
+        <a href="https://www.google.com/">Forgot password?</a>
+        </div>
+        <div className='contGoogle'>
+        <img src={googleSignIn} className="google" />
+        </div>
+        </div>
+
+        <div className='makeAccount'>
+          <Button className='makeAccountButton'>Make Account</Button>
+        </div>
+
+        <div className='continueGuest'>
+          <Button className='guestButton'>Continue as guest</Button>
+        </div>
+
+      </div>
+      </div>
     </div>
   );
 };
